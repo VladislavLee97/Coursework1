184dcb2ae104976307fd19794373d84c3c243437
Vladislav <rk92vlan554@gmail.com>
2023-04-24 03:11:34 +0500

    Сделал Задание

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        String str = "abbcddeffg";
+        char[] ch = str.toCharArray();
+        String result = "";
+        for (int i = 0; i < ch.length - 1; i++) {
+            if (ch[i] != ch[i + 1]) {
+                result += ch[i];
+            }
+        }
+        System.out.println(result);
     }
 }
